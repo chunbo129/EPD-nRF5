59a25e341e4cb693347e4b6f41b9a7d67e48f4ab
chunbo129 <chunbo9@gmail.com>
2026-02-26 17:00:11 +0800

    fix:List_all_S112_FWIDs_explicitly

--- generate_dfu_package.ps1
+++ generate_dfu_package.ps1
@@ -12,9 +12,19 @@ $ErrorActionPreference = "Stop"
 $HEX_FILE = "_build_nrf52/nrf52811_xxaa.hex"
 $OUT_FILE = "_build_nrf52/app_dfu_package.zip"
 $KEY_FILE = "tools/priv.pem"
-$SD_REQ   = "0xFFFE" # Allow any SoftDevice version (0xFFFE)
-# Old: $SD_REQ   = "0x126,0x103" # S112 v7.3.0 (0x126) and v7.2.0 (0x103)
-$APP_VER  = 3        # Application version (increment this for updates)
+# Note: The SoftDevice version check (0xFFFE) failed (Code = 7).
+# This means the Bootloader requires a specific SoftDevice ID to be present in the list,
+# or it thinks we are trying to overwrite the SoftDevice when we shouldn't.
+# We must include the CORRECT SoftDevice ID.
+# Let's try adding ALL known S112 versions to be safe.
+# 0xAE = S112 v6.1.0
+# 0xB0 = S112 v6.1.1
+# 0xB8 = S112 v7.0.0
+# 0xC4 = S112 v7.0.1
+# 0x103 = S112 v7.2.0
+# 0x126 = S112 v7.3.0
+$SD_REQ   = "0x126,0x103,0xC4,0xB8,0xB0,0xAE" 
+$APP_VER  = 4        # Application version (increment this for updates)
 
 # Note: nrfutil (classic) has compatibility issues with Python 3.11+.
 # It is recommended to use Python 3.9 if you encounter errors.
